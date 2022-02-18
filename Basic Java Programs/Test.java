0630f9b6b224a1c8c0e7b8f5c023fa670b869a8a
akash <akash300383@gmail.com>
2022-02-18 12:49:50 +0530

    done

--- Basic Java Programs/Test.java
+++ Basic Java Programs/Test.java
@@ -1,8 +1,21 @@
+import java.util.Scanner;
 class Test {
 	public static void main(String args[]) {
-	System.out.println("Welcome to Java...");
-	System.out.println("Welcome to Java...");
-	System.out.println("Welcome to Java...");
+	Scanner obj = new Scanner(System.in);
+	System.out.println("how many details you want to store");
+	int n = obj.nextInt();
+	int []id = new int[n];
+	String []names=new String[n];
+	System.out.println("Enter id and name one by one");
+	for(int i=0;i<n;i++) {
+		System.out.println("Enter the "+(i+1)+" Id ");
+		id[i]=obj.nextInt();
+		System.out.println("Enter the "+(i+1)+" Name ");
+		names[i]=obj.next();
 	}
-
+	System.out.println("All details are ");
+	for(int i=0;i<n;i++) {
+	System.out.println("id is "+id[i]+" Name is "+names[i]);
+	}
+	}
 }
