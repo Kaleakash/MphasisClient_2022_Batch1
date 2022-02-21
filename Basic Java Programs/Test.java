2efcc1c2a22ba77d54798c35824bb315334655dc
akash <akash300383@gmail.com>
2022-02-21 10:59:12 +0530

    done

--- Basic Java Programs/Test.java
+++ Basic Java Programs/Test.java
@@ -1,21 +1,30 @@
-import java.util.Scanner;
+class Employee {
+	private String name;
+	private float salary;
+	void setValue(String name, float salary) {
+		//name = name;		localvarible = localvariable 	
+		//salary = salary;
+		this.name = name;
+		//this.salary = salary;
+		if(salary<=0) {
+			this.salary = 8000;
+		}else {
+			this.salary  = salary;
+		}
+	}
+	void display() {
+		System.out.println("Name is "+name);
+		System.out.println("Salary is "+salary);
+	}
+}
 class Test {
 	public static void main(String args[]) {
-	Scanner obj = new Scanner(System.in);
-	System.out.println("how many details you want to store");
-	int n = obj.nextInt();
-	int []id = new int[n];
-	String []names=new String[n];
-	System.out.println("Enter id and name one by one");
-	for(int i=0;i<n;i++) {
-		System.out.println("Enter the "+(i+1)+" Id ");
-		id[i]=obj.nextInt();
-		System.out.println("Enter the "+(i+1)+" Name ");
-		names[i]=obj.next();
-	}
-	System.out.println("All details are ");
-	for(int i=0;i<n;i++) {
-	System.out.println("id is "+id[i]+" Name is "+names[i]);
-	}
+	//display();	
+	//salary = 12000;
+	Employee emp = new Employee();
+	//emp.name = "Raj Deep";
+	//emp.salary = -12000;
+	emp.setValue("Raj Deep",12000);
+	emp.display();
 	}
 }
