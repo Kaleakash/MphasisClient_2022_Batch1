1da0f44f7ad6dab84626cd12b5bd7be1e00f41f9
akash <akash300383@gmail.com>
2022-03-04 12:41:57 +0530

    done

--- Phase 2/Maven Project/JDBC_CRUD_Operation/src/com/DemoTest.java
+++ Phase 2/Maven Project/JDBC_CRUD_Operation/src/com/DemoTest.java
@@ -12,10 +12,29 @@ Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/my_dat
 		System.out.println("Database connected...");
 		
 		Statement stmt = con.createStatement();
-		int res = stmt.executeUpdate("insert into employee values(5,'Mahesh',28000)");
+		
+		//Insert Query 
+//		int res = stmt.executeUpdate("insert into employee values(5,'Mahesh',28000)");
+//		if(res>0) {
+//			System.out.println("Record inserted successfully");
+//		}
+		
+//		// Delete Query 
+//		int res = stmt.executeUpdate("delete from employee where id = 5");
+//		if(res>0) {
+//			System.out.println("Record deleted successfully");
+//		}else {
+//			System.out.println("Record not present");
+//		}
+		
+		// updatet Query 
+		int res = stmt.executeUpdate("update employee set salary = 35000 where id = 1");
 		if(res>0) {
-			System.out.println("Record inserted successfully");
+			System.out.println("Record updated successfully");
+		}else {
+			System.out.println("Record not present");
 		}
+		
 		} catch (Exception e) {
 			System.out.println(e);
 		}
