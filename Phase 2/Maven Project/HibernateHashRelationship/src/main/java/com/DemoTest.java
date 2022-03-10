5dda71fe6e9ebb029e84e0c76d67a2d7d6c9d931
akash <akash300383@gmail.com>
2022-03-10 09:35:35 +0530

    done

--- Phase 2/Maven Project/HibernateHashRelationship/src/main/java/com/DemoTest.java
+++ Phase 2/Maven Project/HibernateHashRelationship/src/main/java/com/DemoTest.java
@@ -1,8 +1,10 @@
 package com;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -38,28 +40,53 @@ public class DemoTest {
 //		tran.commit();
 //		System.out.println("Student Record saved....");
 		
-		Trainer t = new Trainer();
-		t.setTid(4);
-		t.setTname("Raju");
-		t.setTech("React JS");
-
-		List<Student> listOfStd = new ArrayList<>();
+//		Trainer t = new Trainer();
+//		t.setTid(4);
+//		t.setTname("Raju");
+//		t.setTech("React JS");
+//
+//		List<Student> listOfStd = new ArrayList<>();
+//		
+//		Student s1 =new Student(103, "Meeta", 24, 4);
+//		Student s2 =new Student(104, "Veeta", 25, 4);
+//		
+//		listOfStd.add(s1);
+//		listOfStd.add(s2);
+//		
+//		t.setListOfStudent(listOfStd);
+//		
+//		tran.begin();
+//				session.save(t);			// only save trainer object. 
+//			//session.save(s1);
+//			//session.save(s2);
+//		tran.commit();
+//		System.out.println("Trainer record saved...");
 		
-		Student s1 =new Student(103, "Meeta", 24, 4);
-		Student s2 =new Student(104, "Veeta", 25, 4);
+		// join using HQL
+//		Query qry= session.createQuery("select t.tname,t.tech,s.sname from Trainer t inner join Student s on t.tid = s.tsid");
+//		List<Object[]> list = qry.list();
+//		Iterator<Object[]> li = list.iterator();
+//		while(li.hasNext()) {
+//			Object obj[] = li.next();
+//			System.out.println("Trainer name "+obj[0]+" Tech "+obj[1]+" Student Name "+obj[2]);
+//		}
+		// Simple SQL Query 
+		Query qry= session.createNativeQuery("select * from employee");
+		List<Object[]> list = qry.list();
+		Iterator<Object[]> li = list.iterator();
+		while(li.hasNext()) {
+			Object obj[] = li.next();
+			System.out.println("Id "+obj[0]+" Name "+obj[1]+" Salary "+obj[2]);
+		}
 		
-		listOfStd.add(s1);
-		listOfStd.add(s2);
-		
-		t.setListOfStudent(listOfStd);
-		
-		tran.begin();
-				session.save(t);			// only save trainer object. 
-			//session.save(s1);
-			//session.save(s2);
-		tran.commit();
-		System.out.println("Trainer record saved...");
-		
+		//joing using SQL 
+//		Query qry= session.createNativeQuery("select t.tname,t.tech,s.sname from trainer t inner join student s on t.tid = s.tsid");
+//		List<Object[]> list = qry.list();
+//		Iterator<Object[]> li = list.iterator();
+//		while(li.hasNext()) {
+//			Object obj[] = li.next();
+//			System.out.println("Trainer name "+obj[0]+" Tech "+obj[1]+" Student Name "+obj[2]);
+//		}
 	}
 
 }
