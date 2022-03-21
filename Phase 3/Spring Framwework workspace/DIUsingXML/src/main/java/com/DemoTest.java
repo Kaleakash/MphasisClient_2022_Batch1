05d082270253ff2f5b63b15acd448a6e2dfdcb79
Akash <akash300383@gmail.com>
2022-03-21 10:10:29 +0530

    done

--- Phase 3/Spring Framwework workspace/DIUsingXML/src/main/java/com/DemoTest.java
+++ Phase 3/Spring Framwework workspace/DIUsingXML/src/main/java/com/DemoTest.java
@@ -16,17 +16,36 @@ public class DemoTest {
 													// that interface implementation class 
 													// is XmlBeanFactory
 		BeanFactory bean = new XmlBeanFactory(new ClassPathResource("beans.xml"));
-		Employee e1 = (Employee)bean.getBean("emp1");
-		e1.display();
 		
-		Employee ee1 = (Employee)bean.getBean("emp1");
-		ee1.display();
+//		Employee e1 = (Employee)bean.getBean("emp1");
+//		e1.display();
+//		
+//		Employee ee1 = (Employee)bean.getBean("emp1");
+//		ee1.display();
+//		
+//		Employee e2 = (Employee)bean.getBean("emp2");
+//		e2.display();
+//		
+//		Employee ee2 = (Employee)bean.getBean("emp2");
+//		ee2.display();
 		
-		Employee e2 = (Employee)bean.getBean("emp2");
-		e2.display();
+//		Employee e1 = (Employee)bean.getBean("emp1");
+//		System.out.println(e1);			// it will call toString() method. 
+//		Employee e2 = (Employee)bean.getBean("emp2");
+//		System.out.println(e2);			// it will call toString() method.
+//		
+//		Employee e3 = (Employee)bean.getBean("emp3");
+//		System.out.println(e3);			// it will call toString() method. 
+//		
+//		Employee e4 = (Employee)bean.getBean("emp4");
+//		System.out.println(e4);			// it will call toString() method. 
+//		
+//		Employee e5 = (Employee)bean.getBean("emp5");
+//		System.out.println(e5);	
 		
-		Employee ee2 = (Employee)bean.getBean("emp2");
-		ee2.display();
+		Employee emp = (Employee)bean.getBean("employee");
+		System.out.println(emp);
 	}
+	
 
 }
