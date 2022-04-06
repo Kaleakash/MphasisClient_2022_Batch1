1fa9242cc97a52cf6bd021eb8d8ef6390b1bbbb9
Akash <akash300383@gmail.com>
2022-04-06 12:50:08 +0530

    done

--- Phase 4/UI Technologies/Angular Project/demo-app/src/app/app.module.ts
+++ Phase 4/UI Technologies/Angular Project/demo-app/src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {ChildComponent} from './child.component'
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
+    AppComponent,ChildComponent, HeaderComponent, FooterComponent
   ],
   imports: [
     BrowserModule
