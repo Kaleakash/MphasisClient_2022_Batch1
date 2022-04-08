7262a68f99018ed6e4a4fb1385c84915cfb15804
Akash <akash300383@gmail.com>
2022-04-08 12:22:58 +0530

    done

--- Phase 4/UI Technologies/Angular Project/test-app-demo/src/app/app.module.ts
+++ Phase 4/UI Technologies/Angular Project/test-app-demo/src/app/app.module.ts
@@ -1,16 +1,19 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { ExamComponent } from './exam/exam.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExamComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,HttpClientModule,FormsModule
   ],
-  providers: [],
+  providers: [],      // this place only for service class details. 
   bootstrap: [AppComponent]
 })
 export class AppModule { }
