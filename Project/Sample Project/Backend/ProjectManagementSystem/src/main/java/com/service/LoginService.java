cae676c15d3247d39e76179e2aef9c72e4d5d80e
Akash <akash300383@gmail.com>
2022-04-26 10:32:35 +0530

    done

---
+++ Project/Sample Project/Backend/ProjectManagementSystem/src/main/java/com/service/LoginService.java
@@ -0,0 +1,49 @@
+package com.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.bean.Login;
+import com.repository.LoginRepository;
+
+@Service
+public class LoginService {
+
+	@Autowired
+	LoginRepository loginRepository;
+	
+	
+	public String signUp(Login login) {
+		
+			boolean res = loginRepository.existsById(login.getEmailid());
+			if(res) {
+				return "Account not create";
+			}else {
+				
+				if(login.getTypeOfUser().equals("admin")){
+					return "You can't create admin account";
+				}
+				else {
+					loginRepository.save(login);
+					return "Account created successfully";	
+				}
+			}
+	}
+	
+	public String signIn(Login login) {
+		if(login.getTypeOfUser().equals("admin")) {
+			if(loginRepository.checkLoginDetails(login.getEmailid(), login.getPassword())!=null) {
+				return "admin login successfully";
+			}else {
+				return "failure";
+			}
+		}else {
+			if(loginRepository.checkLoginDetails(login.getEmailid(), login.getPassword())!=null) {
+				return "user login successfully";
+			}else {
+				return "failure";
+			}
+		}
+		
+	}
+}
